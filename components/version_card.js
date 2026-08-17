c6021aad74c441455fa342df29abead1dbe1c9d0
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2026-08-17 15:17:50 -0400

    Download page scaling for screen sizes

--- components/version_card.js
+++ components/version_card.js
@@ -8,7 +8,8 @@ class VersionCard extends HTMLElement {
             <link rel="stylesheet" href="../style.css">
             
             <div class="version-entry">
-                <h1 class="version-title"></h1>
+                <h1 class="great-big-world-title">Great Big World</h1>
+                <h1 class="version-number"></h1>
                 <gbw-version-type-label class="version-type"></gbw-version-type-label>
                 <a class="version-url">
                     <img src="../assets/icons/download.png" class="download-icon" alt="Download">
@@ -17,14 +18,14 @@ class VersionCard extends HTMLElement {
             </div>
         `;
 
-        this.versionTitle = shadow.querySelector('.version-title');
+        this.versionNumber = shadow.querySelector('.version-number');
         this.versionType = shadow.querySelector('.version-type');
         this.versionDownloadCount = shadow.querySelector('.version-download-count');
         this.versionUrl = shadow.querySelector('.version-url');
     }
 
     connectedCallback() {
-        this.versionTitle.textContent = this.getAttribute('version-title')
+        this.versionNumber.textContent = this.getAttribute('version-number')
         this.versionType.textContent = this.getAttribute('version-type')
         this.versionType.setAttribute('version-type', this.getAttribute('version-type'))
         this.versionDownloadCount.textContent = this.getAttribute('version-downloads')
